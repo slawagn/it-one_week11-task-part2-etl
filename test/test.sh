db9405aa0f870a7fff9aa8ae225e1b350a7ccff6
slawagn <slawagn@gmail.com>
2026-02-21 04:47:57 +0300

    Squashed commit of the following:
    commit 7b2113944107fb6ee0e1e0c26808c607c12a0e6b
    Author: slawagn <slawagn@gmail.com>
    Date:   Fri Feb 20 18:35:37 2026 +0300
    
        remove unneeded -it flags
    
    commit af9f96a4ba309be0e2a8f1d66ab235785f43be3f
    Author: slawagn <slawagn@gmail.com>
    Date:   Fri Feb 20 18:27:25 2026 +0300
    
        another attempt
    
    commit 997e7a09fe9803d2f32e7524cf3a6fd68cabdb1a
    Author: slawagn <slawagn@gmail.com>
    Date:   Fri Feb 20 18:25:11 2026 +0300
    
        disable tls for docker
    
    commit 6bf828df1ab14b9851ba8b4ec4a6eaa73ec01905
    Author: slawagn <slawagn@gmail.com>
    Date:   Fri Feb 20 18:23:35 2026 +0300
    
        i really don't know
    
    commit 9d091446d0603c091694f1d388962e5cde5da2c3
    Author: slawagn <slawagn@gmail.com>
    Date:   Fri Feb 20 18:19:02 2026 +0300
    
        not sure what i'm doing at this point
    
    commit a6618aa18f7b641f744525a70f699990e03adb44
    Author: slawagn <slawagn@gmail.com>
    Date:   Fri Feb 20 18:16:01 2026 +0300
    
        another attempt at making the pipeline work
    
    commit 1d1d8a9b30cc529827c8a97eb096a55f401fa8b6
    Author: slawagn <slawagn@gmail.com>
    Date:   Fri Feb 20 15:14:06 2026 +0300
    
        set docker:dind as the image
    
    commit 35d72a8dffa3ca518218ef2c7119aa2f123f6606
    Author: slawagn <slawagn@gmail.com>
    Date:   Fri Feb 20 15:10:28 2026 +0300
    
        fix attempt
    
    commit 6e5d23af27277fcb37048b3e1f165be07100e24c
    Author: slawagn <slawagn@gmail.com>
    Date:   Fri Feb 20 15:08:13 2026 +0300
    
        fix typo
    
    commit 744780f63359235484a31e0a9862081ebe3879d2
    Author: slawagn <slawagn@gmail.com>
    Date:   Fri Feb 20 15:05:40 2026 +0300
    
        attempt 1 of creating a ci pipeline

--- test/test.sh
+++ test/test.sh
@@ -2,6 +2,6 @@ docker compose up -d || { docker compose down; exit 1; }
 
 docker container exec test-spark-env sh -c "spark-submit  --packages org.postgresql:postgresql:42.7.9 /tmp/spark-env/spark-etl.py" || { docker compose down; exit 1; }
 
-docker container exec -u postgres -it test-db-dest bash -c "psql -v ON_ERROR_STOP=on -f /tmp/test-db-dest-env/test.sql" || { docker compose down; exit 1; }
+docker container exec -u postgres test-db-dest bash -c "psql -v ON_ERROR_STOP=on -f /tmp/test-db-dest-env/test.sql" || { docker compose down; exit 1; }
 
 docker compose down
